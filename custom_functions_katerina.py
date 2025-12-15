0166989b9040525a8ca4b14a455597b61d8fa825
Katerina Psallida <katherinepsallida@gmail.com>
2025-12-15 21:38:42 +0200

    Doing Tensor

--- custom_functions_katerina.py
+++ custom_functions_katerina.py
@@ -12,3 +12,21 @@ def find_outliers(dX):
     dX['is_outlier'] = (dX['z_score'].abs() > 3).astype(int)
     
     return dX
+
+#####################################################################################
+ef print_timeseries(df, title):
+    """
+    Plot all time series with consistent custom colors.
+    """
+    
+       
+    #Pass the ordered list to the plot function
+    df.plot(ax=plt.gca(), color=plot_colors) 
+    
+    plt.title(title)
+    plt.xlabel("Date")
+    plt.ylabel("Load (MW)")
+    plt.legend(title="Country")
+    plt.tight_layout()
+    plt.show()
+
