1cfea39591fead6760a970d4b2cc4441ae878d58
Katerina Psallida <katherinepsallida@gmail.com>
2025-12-17 11:05:54 +0200

    final

---
+++ custom_functions.py
@@ -0,0 +1,29 @@
+import pandas as pd  # For creating the DataFrame
+
+def find_outliers_zscore(dX):
+    dX = dX.copy()
+    
+    dX['mean'] = dX.groupby(['region', 'indicator_id'])['value'].transform('mean')
+    dX['std']  = dX.groupby(['region', 'indicator_id'])['value'].transform('std')
+    
+    dX['z_score'] = (dX['value'] - dX['mean']) / dX['std']
+    
+    # Identify Outliers (Threshold > 3)
+    dX['is_outlier'] = (dX['z_score'].abs() > 3).astype(int)
+    
+    return dX
+
+#####################################################################################
+def print_timeseries(df, title):
+    """Plot all time series with consistent custom colors.
+    """
+    #Pass the ordered list to the plot function
+    df.plot(ax=plt.gca(), color=plot_colors) 
+        
+    plt.title(title)
+    plt.xlabel("Date")
+    plt.ylabel("Load (MW)")
+    plt.legend(title="Country")
+    plt.tight_layout()
+    plt.show()
+
